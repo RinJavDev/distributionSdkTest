03b645d611dd2abfb0677c12c4e14bedc3f6c51a
r.akhmadullin <r.akhmadullin@vk.team>
2023-10-18 19:16:05 +0300

    Revert "initail commit for rep"
    This reverts commit de269726119a6fa4beefeb50c914405051fd8453.

--- MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/MainActivity.java
+++ MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/MainActivity.java
@@ -1,9 +1,10 @@
 package com.xxmassdeveloper.mpchartexample;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends Activity
+public class MainActivity extends AppCompatActivity
 {
 
 	@Override
